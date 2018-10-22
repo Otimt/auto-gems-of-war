499a2106383ba01f1beb9fb024ef951f0cf0c2fd
Otimt <ddd-1-ddd@163.com>
2018-10-22 19:50:59 +0800

    service 窗口排版

--- foo/window_service.py
+++
@@ -1,8 +0,0 @@
-import socket
-ip_port = ('127.0.0.1',9999)
-sk = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0)
-sk.bind(ip_port)
-
-while True:
-    data = sk.recv(1024)
-    print(data.decode())
