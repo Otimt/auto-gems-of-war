0abab82665afa64a063e97b6186cb06b2b36ff4d
Otimt <ddd-1-ddd@163.com>
2018-11-09 08:45:03 +0800

    python 写web服务,接受url参数 准备执行 socket

--- foo/service/web.py
+++ foo/service/web.py
@@ -1,8 +1,41 @@
-#!/usr/bin/python
-# encoding : utf-8
+#!/usr/bin/env python
+#coding=utf-8
+#author:naiveloafer
+#date:2012-06-07
+ 
 from flup.server.fcgi import WSGIServer
-def myapp(environ, start_response):
+import json
+ 
+def get_environ_param(environ):
+    rquest_method = environ["REQUEST_METHOD"]
+    str = "rquest_method:" + rquest_method + "\r\n"
+    query_string = environ["QUERY_STRING"]
+    str += ",query_string:" + query_string + "\r\n"
+    #script_filename = environ["SCRIPT_FILENAME"]
+    #str += ",script_filename:" + script_filename + "\r\n"
+    #script_name = environ["SCRIPT_NAME"]
+    #str += ",script_name:" + script_name + "\r\n"
+    #rquest_uri = environ["REQUEST_URI"]
+    #str += ", rquest_uri:" + rquest_uri + "\r\n"
+    #remote_addr = environ["REMOTE_ADDR"]
+    #str += ",remote_addr:" + remote_addr + "\r\n"
+    #remote_port = environ["REMOTE_PORT"]
+    #str += ",remote_port:" + remote_port + "\r\n"
+    #
+    #data = environ["wsgi.input"].read()
+    #str += ", data:" + data + "\r\n"
+    print(query_string)
+    obj = dict([x.split('=',1) for x in query_string.split('&')])
+    return obj
+ 
+def naiveloafer_app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['Hello World!\n']
+    param_dict = get_environ_param(environ)
+    res = json.dumps(param_dict)
+    return [res]
+ 
+
+
+    
 if __name__  == '__main__':
-    WSGIServer(myapp,bindAddress=('127.0.0.1',8090)).run()
+    WSGIServer(naiveloafer_app,bindAddress=('127.0.0.1',8090)).run()
